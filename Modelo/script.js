f48f3e87fd32eac444436f2f3cecbb0e471a40d5
Larisse Lima <larisse.lima2@gmail.com>
2020-07-10 16:20:03 -0300

    Adicona exercíco Verificador de Idade

--- Modelo/script.js
+++ Modelo/script.js
@@ -1,9 +1,9 @@
 function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    //var hora = 9
+    const msg = window.document.getElementById('msg')
+    const img = window.document.getElementById('imagem')
+    const data = new Date()
+    const hora = data.getHours()
+    //const hora = 22
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 0 && hora < 12) {
